863cd218b818d8ba3443078d7a8bb83cee3015ad
Zac Beemer <zeeveener@gmail.com>
2018-01-21 10:57:25 -0700

    Create OrderStatus enum

---
+++ src/main/java/com/zeeveener/crypto/exchange/api/binance/constants/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.zeeveener.crypto.exchange.api.binance.constants;
+
+public enum OrderStatus {
+    NEW,
+    PARTIALLY_FILLED,
+    FILLED,
+    CANCELED,
+    PENDING_CANCEL,
+    REJECTED,
+    EXPIRED
+}
